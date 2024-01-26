e5684eab0884ad4e4dbb61b67dc1a3afa2915435
nissim25100 <nissim25100@gmail.com>
2024-01-26 11:45:17 +0200

    Nissim.javawas added

--- Nissim.java
+++ Nissim.java
@@ -1,7 +1,7 @@
 public class Nissim{
 	public static void main(String[] args){
 		for (int i = 0; i <= 10; i++){
-			System.out.println("nissim hasckor..."+i);
+			System.out.println("nissim hasckor...goat"+i);
 		}
 	}
 }		
